89439cb02e2379485ec0ee1863f7b859747f3d01
annedazh <67046331+annedazh@users.noreply.github.com>
2020-06-23 19:30:57 +0900

    ggg

--- Project01/src/RE/GroupJoin.java
+++ Project01/src/RE/GroupJoin.java
@@ -1,24 +1,27 @@
 package RE;
 
 import java.awt.EventQueue;
+import java.awt.Image;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GroupJoin {
 
 	private JFrame frame;
-	private JTextField txt_id;
-	private JPasswordField txt_pw1;
-	private JPasswordField txt_emailID;
-	private JTextField txt_Uni;
-	private JTextField txt_Major;
-	private JButton btn_Cancel;
+	private JTextField txt_id1;
+	private JTextField txt_id2;
 
 	/**
 	 * Launch the application.
@@ -48,59 +51,87 @@ public class GroupJoin {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 759,541);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+		
+		
+		//이미지입력
+		String path = "C:\\Users\\SMHRD\\git\\Project01\\Project01\\src\\icon\\회원가입.png";
+		Image image = new ImageIcon(path).getImage();
 		frame.getContentPane().setLayout(null);
 		
+		
 		JPanel panel = new JPanel();
-		panel.setBounds(12, 10, 410, 241);
+		panel.setBounds(0, 0, 759,541);
 		frame.getContentPane().add(panel);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setBounds(144, 466, 388, 35);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				GroupLogin01.main(null);
+			}
+		});
 		panel.setLayout(null);
+		panel.add(btnNewButton);
 		
-		txt_id = new JTextField();
-		txt_id.setText("\uC544\uC774\uB514");
-		txt_id.setBounds(146, 35, 116, 21);
-		panel.add(txt_id);
-		txt_id.setColumns(10);
+		// 버튼 투명도
+		btnNewButton.setOpaque(false);
+		btnNewButton.setBorder(null);
+		btnNewButton.setContentAreaFilled(false);
 		
-		JButton btn_Complete = new JButton("\uC644\uB8CC");
-		btn_Complete.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
-				GroupLogin01.main(null);
-			}
-		});
-		btn_Complete.setBounds(119, 208, 97, 23);
-		panel.add(btn_Complete);
-		
-		txt_pw1 = new JPasswordField();
-		txt_pw1.setBounds(146, 73, 116, 21);
-		panel.add(txt_pw1);
-		
-		txt_emailID = new JPasswordField();
-		txt_emailID.setText("");
-		txt_emailID.setBounds(146, 104, 116, 21);
-		panel.add(txt_emailID);
-		
-		txt_Uni = new JTextField();
-		txt_Uni.setBounds(146, 137, 116, 21);
-		panel.add(txt_Uni);
-		txt_Uni.setColumns(10);
-		
-		txt_Major = new JTextField();
-		txt_Major.setBounds(146, 177, 116, 21);
-		panel.add(txt_Major);
-		txt_Major.setColumns(10);
-		
-		btn_Cancel = new JButton("\uCDE8\uC18C");
-		btn_Cancel.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
-				GroupLogin01.main(null);
-			}
-		});
-		btn_Cancel.setBounds(220, 208, 97, 23);
-		panel.add(btn_Cancel);
-	}
+				JLabel lbl_image = new JLabel(new ImageIcon(image.getScaledInstance(750,660, Image.SCALE_FAST)));
+				lbl_image.setBounds(0, 0, 747, 501);
+				panel.add(lbl_image);
+				
+				txt_id1 = new JTextField();
+				txt_id1.setText("ID");
+				txt_id1.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						txt_id1.setText(null);
+					}
+				});
 
+				txt_id1.setBounds(167, 0, 462, 63);
+				panel.add(txt_id1);
+				txt_id1.setColumns(10);
+				
+				
+				
+				
+				
+				
+				
+				
+				
+		
+		
+	
+
+//		JLabel lbl_image = new JLabel(new ImageIcon(image.getScaledInstance(300, 350, Image.SCALE_FAST)));
+//		lbl_image.addAncestorListener(new AncestorListener() {
+//			public void ancestorAdded(AncestorEvent arg0) {
+//			}
+//
+//			public void ancestorMoved(AncestorEvent arg0) {
+//			}
+//
+//			public void ancestorRemoved(AncestorEvent arg0) {
+//			}
+//		});
+		
+		
+//		   String path = "C:\\Users\\SMHRD\\Desktop\\java_1\\하나둘셋야\\src\\image\\pay_notime.jpg";
+//		      Image image = new ImageIcon(path).getImage();
+//		      panel.setLayout(null);
+	}
 }
